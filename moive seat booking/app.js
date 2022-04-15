43db236744fe6a17d00de297fb0b0a7f0186fb77
William McColl <william.mccoll@bipsolutions.com>
2022-04-15 19:19:24 +0100

    local storage added for seat booking app

--- moive seat booking/app.js
+++ moive seat booking/app.js
@@ -4,11 +4,28 @@ const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
 
+populateUI()
+
 let ticketPrice = +movieSelect.value
+
+//  save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex)
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
+
 
 // update total price and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat))
+
+
+    // local storage
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
 
     const selectedSeatsCount = selectedSeats.length
 
@@ -16,15 +33,38 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice
 }
 
-// movie select event
-movieSelect.addEventListener('change', (e) => {
-    ticketPrice = +e.target.value
 
+ // get data from local storage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected')
+            }
+        })
+    }
+
+   const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+
+   if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex
+   }
+}
+
+
+// movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value
+    setMovieData(e.target.selectedIndex, e.target.value)
     updateSelectedCount()
 })
 
+
+
 // seat click event
-container.addEventListener('click', (e) => {
+container.addEventListener('click', e => {
     if(
         e.target.classList.contains('seat') && 
         !e.target.classList.contains('occupied')
@@ -34,3 +74,6 @@ container.addEventListener('click', (e) => {
         updateSelectedCount()
     }
 })
+
+// init count and total
+updateSelectedCount()
